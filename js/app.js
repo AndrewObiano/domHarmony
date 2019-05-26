2435ae6dca62fde6ac83d458eea9aa3f33d81155
Andrew Obiano <andrew.obiano@gmail.com>
2019-05-25 16:57:36 -1000

    finished

--- js/app.js
+++ js/app.js
@@ -26,21 +26,56 @@ data.appendChild(divElem); // location id, append (what)
 
 //6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
 
+var divHobby = document.createElement('div');
+divHobby.id = 'hobbies';
+divHobby.innerHTML = 'Long romantic walks on the beach, candle light Big Mac dinners and tormenting the  King.';
+data.appendChild(divHobby);
+
 //7. In the div id data section, create a div with an id of location and give it the following information: Honolulu, HI.
+
+var divLocation = document.createElement('div');
+divLocation.id = 'location';
+divLocation.innerHTML = 'Honolulu, HI';
+data.appendChild(divLocation);
 
 //8. In the div id data section, create a div with an id of wants and give it the following information: Looking for a Mrs. McDonald.
 
+var wantsElem = document.createElement('div');
+wantsElem.id = 'Wants';
+wantsElem.innerHTML = 'Looking for a Mrs. McDonald';
+data.appendChild(wantsElem);
+
 //9. In the div id profile section, create a paragraph element with an id of pro2 and create a your own profile for Ronald (at least 2 sentences).
+
+var newPro = document.createElement('p');
+newPro.id = 'pro2';
+newPro.innerHTML = 'Im a very sad Ronald right now.';
+profile.appendChild(newPro);
 
 //10. In the div id matches section, target the first div element with the class name of firstName and change Name to Wendy.
 
+var nameElem = document.getElementsByClassName('firstName');
+nameElem[0].innerHTML = 'Wendy';
+
 //11. In the div id matches section, target the first div element with the class name of otherAge and change Age to 48.
+
+var ageElem = document.getElementsByClassName('otherAge');
+ageElem[0].innerHTML = '48';
 
 //12. In the div id matches section, target the first div element with the class name of status and change Status to Single Mother.
 
+var statusElem = document.getElementsByClassName('status');
+statusElem[0].innerHTML = 'Single Mother';
+
 //13. In the div id matches section, target the second div element with the class name of firstName and change Name to Peko Chan.
 
+var nameElem2 = document.getElementsByClassName('firstName');
+nameElem2[1].innerHTML = 'Peko Chan';
+
 //14. In the div id matches section, target the second div element with the class name of otherAge and change Age to 68.
+
+var ageElem2 = document.getElementsByClassName('otherAge');
+ageElem2[1].innerHTML = '68';
 
  //Final Boss Create your own profile into the page:
  //change image to match your profile
@@ -49,6 +84,16 @@ data.appendChild(divElem); // location id, append (what)
  //div with class name of status
  //div with class anem of Motto
 
+document.getElementsByClassName('other')[2].src = 'https://cdn.vox-cdn.com/thumbor/eReqmSD1nz4TXHXzbPslJgRcKRQ=/0x0:3840x2400/1200x800/filters:focal(1613x893:2227x1507)/cdn.vox-cdn.com/uploads/chorus_image/image/62630829/The_Walking_Dead_TV_502082_3840x2400.0.jpg';
 
+var nameElem3 = document.getElementsByClassName('firstName');
+nameElem3[2].innerHTML = 'Andrew Obiano';
 
+var ageElem3 = document.getElementsByClassName('otherAge');
+ageElem3[2].innerHTML = '25';
 
+var statusElem3 = document.getElementsByClassName('status');
+statusElem3[2].innerHTML = 'Fly';
+
+var motto3 = document.getElementsByClassName('motto');
+motto3[2].innerHTML = 'If can, can.';
